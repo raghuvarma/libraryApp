382425ebe9054773e8fef0a2e3d8dcf6c0326678
Raghu Varma <raghuvarma@masymbol.com>
2014-11-09 16:44:06 +0530

    modified student_book model

--- app/models/student_book.rb
+++ app/models/student_book.rb
@@ -1,5 +1,4 @@
-class StudentBooks < ActiveRecord::Base
+class StudentBook < ActiveRecord::Base
 	belongs_to :student
 	belongs_to :book
-
 end
--- test/models/student_book_test.rb
+++ test/models/student_book_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StudentBooksTest < ActiveSupport::TestCase
+class StudentBookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
