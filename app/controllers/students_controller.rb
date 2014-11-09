406c00bf9f4ed1f6d0f76c7b2685c85747edcd98
Raghu Varma <raghuvarma@masymbol.com>
2014-11-09 13:55:29 +0530

    updated students controller with all crud functions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,9 +20,32 @@ class StudentsController < ApplicationController
 		@student = Student.find(params[:id])
 	end
 
+	def edit
+		@student = Student.find(params[:id])
+	end
+
+	def update
+	  @student = Student.find(params[:id])
+	 
+	  if @student.update(student_params)
+	    redirect_to @student
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+	  @student = Student.find(params[:id])
+	  @student.destroy
+	 
+	  redirect_to students_path
+	end
+
 	private
   def student_params
     params.require(:student).permit(:name)
   end
+
+
 
 end
